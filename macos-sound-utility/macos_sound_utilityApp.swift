004d2504630a66e4443dd370ca74591f6785e0c5
Daeram Chung <daeram.chung@gmail.com>
2023-05-15 18:40:28 +0900

    Initial Commit

---
+++ macos-sound-utility/macos_sound_utilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_sound_utilityApp.swift
+//  macos-sound-utility
+//
+//  Created by Daeram Chung on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct macos_sound_utilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
